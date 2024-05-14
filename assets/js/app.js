901cd5e1075f1a3314451362b0ad75704d498f50
internfossphorus <107787520+FarooqProProgrammer@users.noreply.github.com>
2024-05-14 18:07:39 +0500

    Updated

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $('.recent__project__slider').slick({
+        centerMode: true,
+        slidesToShow: 1,
+        centerPadding:'500px',
+        dots:true
+        
+        
+    });
+
+    function updateClasses() {
+
+        $('.recent__project__slider .slick-slide').removeClass('previous-element next-element');
+        $('.recent__project__slider .slick-slide').css('padding','0px');
+        const $center = $('.recent__project__slider .slick-center');
+        $center.css('width', '903px'); 
+        // Add classes to the new previous and next elements
+        $('.recent__project__slider .slick-center').prev().addClass('previous-element position-relative ');
+        $('.recent__project__slider .slick-center').next().addClass('next-element position-relative ');
+
+        // $('.recent__project__slider .slick-center').prev().css('padding-right','40px');
+        // $('.recent__project__slider .slick-center').next().css('padding-left','40px');
+    
+     
+    }
+
+    // Initial class update
+    updateClasses();
+
+    // Update classes after each slide change
+    $('.recent__project__slider').on('afterChange', function(event, slick, currentSlide) {
+        updateClasses();
+   
+
+    });
+    
+    // $('.slick-current').addClass('px-6')
+    $('.slick-dots button').text('')
+  
+});
